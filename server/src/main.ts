6e06477d5a28bd89dfbe7e86c7bea57f3b3cac18
yedu.k <yedu345@gmail.com>
2025-11-24 15:10:28 +0530

    headers

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,6 +9,12 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://ten-bay-beta.vercel.app/');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
   await app.listen(3000);
 }
 bootstrap();
