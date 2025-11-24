3c28a44542149b7d09c1875d2c5ac70f24245123
yedu.k <yedu345@gmail.com>
2025-11-24 13:25:32 +0530

    app

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,24 @@
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const express = require('express');
+const serverless = require('serverless-http');
+
+const expressApp = express();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.enableCors({
     origin: ['http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.init();
 }
-bootstrap();
+
+bootstrap().catch(err => {
+  console.error('Nest bootstrap error', err);
+});
+
+export const handler = serverless(expressApp);
+
