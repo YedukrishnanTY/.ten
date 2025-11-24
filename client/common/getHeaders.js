25bf207ea205550c6759400745c829e91b2a8fed
yedu.k <yedu345@gmail.com>
2025-11-24 15:15:33 +0530

    cors

--- client/common/getHeaders.js
+++ client/common/getHeaders.js
@@ -2,7 +2,6 @@ const getHeaders = () => {
     return {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        'Access-Control-Allow-Origin': true,
     }
 }
 
--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://ten-bay-beta.vercel.app/');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
     next();
